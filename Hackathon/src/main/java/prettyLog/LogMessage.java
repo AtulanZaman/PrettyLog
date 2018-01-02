227e9a4f50aa006f7c9448fe7d1d0a9e0ed705c5
Atulan Zaman <atulan.zaman@nexj.com>
2018-01-02 09:35:14 -0500

    Partial solution with WebSockets

---
+++ Hackathon/src/main/java/prettyLog/LogMessage.java
@@ -0,0 +1,15 @@
+package prettyLog;
+
+public class LogMessage {
+	private String log;
+
+	public LogMessage(){}
+
+	public LogMessage (String log){
+		this.log = log;
+	}
+
+	public String getLog() {
+        return this.log;
+    }
+}
