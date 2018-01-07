83a52015c86881853f2e5d240ab881743ab1b100
Atulan Zaman <atulan.zaman@nexj.com>
2018-01-07 16:56:34 -0500

    Streaming logs!

---
+++ SkimLog/src/main/java/bk/tailfile/web/TailController.java
@@ -0,0 +1,17 @@
+package bk.tailfile.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/files")
+public class TailController {
+
+	@RequestMapping("/home")
+	public String home(Model model) {
+		model.addAttribute("filename", "C:/Work/log/out.txt");
+		return "files/home";
+	}
+
+}
