768f863028c3cd84d2c230492bb935acf34fe00d
Celia Lin <celia.lin@nexj.com>
2018-01-11 19:08:55 -0500

    Navbar in trunk

--- SkimLog/test/echo.sh
+++ SkimLog/test/echo.sh
@@ -1,3 +1,6 @@
 echo "; 2018-01-03 23:49:59,473 DEBUG [nexj.core.persistence.sql.SQLAdapter] Retrieved 1 instance(s) of TaskEntity in 0 ms" >> /c/Work/log/out.txt
 echo "   readable=#t," >> /c/Work/log/out.txt
+echo "   readable=#t," >> /c/Work/log/out.txt
+echo "   readable=#t," >> /c/Work/log/out.txt
+echo "   readable=#t," >> /c/Work/log/out.txt
 echo "; 2018-01-03 23:49:59,474 DEBUG [nexj.model.class.UserPerson] Invoking Event UserPerson.:oid()" >> /c/Work/log/out.txt
