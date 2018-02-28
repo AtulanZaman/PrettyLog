61e9b001f2f7560c5c6ed0c981367d654e9645d0
Atulan Zaman <atulan.zaman@nexj.com>
2018-02-27 22:39:35 -0500

    adding Request mapping to add file name. Need to do this using a model controller rather than using request mapping.

--- SkimLog/src/main/java/bk/tailfile/web/TailController.java
+++ SkimLog/src/main/java/bk/tailfile/web/TailController.java
@@ -3,15 +3,26 @@ package bk.tailfile.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @Controller
 @RequestMapping("/files")
 public class TailController {
+	String filename = "C:/Work/log/out.txt";
 
 	@RequestMapping("/home")
 	public String home(Model model) {
-		model.addAttribute("filename", "C:/Work/log/out.txt");
 		return "files/home";
 	}
 
+	@RequestMapping(value = "/submit", method = RequestMethod.POST)
+	public String submit(
+		@RequestParam("filename") String filename
+	){
+		System.out.println(filename);
+		this.filename = filename;
+		return "files/home";
+	}
 }
